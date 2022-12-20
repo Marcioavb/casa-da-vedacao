1fbd264695f8c7c44306009fa725c52dd3002dc6
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-20 14:09:13 -0300

    inicio do projeto

---
+++ src/main/java/br/com/casadavedao/casadavedacao/CasaDaVedacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.casadavedao.casadavedacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDaVedacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDaVedacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casadavedao/casadavedacao/CasaDaVedacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casadavedao.casadavedacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDaVedacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
