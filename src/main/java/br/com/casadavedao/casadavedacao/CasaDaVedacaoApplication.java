8d15fbc717330738cc56ecca487f45f6b56b2019
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-21 10:04:56 -0300

    implementa mapeamento da class CasaDaVedacaoAppication

--- src/main/java/br/com/casadavedao/casadavedacao/CasaDaVedacaoApplication.java
+++ src/main/java/br/com/casadavedao/casadavedacao/CasaDaVedacaoApplication.java
@@ -3,7 +3,11 @@ package br.com.casadavedao.casadavedacao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class CasaDaVedacaoApplication {
 	
