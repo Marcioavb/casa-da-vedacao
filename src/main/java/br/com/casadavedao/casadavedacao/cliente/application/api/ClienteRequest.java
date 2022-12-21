62a377a657ec3506429220fc80072a102be30ed9
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-21 09:40:48 -0300

    implementa clienteRequest e clienteResponse

--- src/main/java/br/com/casadavedao/casadavedacao/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/casadavedao/casadavedacao/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,32 @@
 package br.com.casadavedao.casadavedacao.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.casadavedao.casadavedacao.cliente.domain.Avaliacao;
+import br.com.casadavedao.casadavedacao.cliente.domain.Sexo;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@NotBlank
+	private String rg;
+	@CPF
+	private String cpf;
+	private Avaliacao avaliacao;
 }
